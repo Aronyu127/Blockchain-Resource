ec112c2fd125ac968b491fa64f06e002ba830eb5
aronyu <h0935674938@hotmail.com>
2023-11-24 11:42:33 +0800

    Implement Section3 Liquidator

--- section3/FlashSwapPractice/contracts/Liquidator.sol
+++ section3/FlashSwapPractice/contracts/Liquidator.sol
@@ -9,11 +9,19 @@ import { IUniswapV2Factory } from "v2-core/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Router01 } from "v2-periphery/interfaces/IUniswapV2Router01.sol";
 import { IWETH } from "v2-periphery/interfaces/IWETH.sol";
 import { IFakeLendingProtocol } from "./interfaces/IFakeLendingProtocol.sol";
+import "forge-std/Test.sol";
+
 
 // This is liquidator contract for testing,
 // all you need to implement is flash swap from uniswap pool and call lending protocol liquidate function in uniswapV2Call
 // lending protocol liquidate rule can be found in FakeLendingProtocol.sol
 contract Liquidator is IUniswapV2Callee, Ownable {
+    struct CallbackData {
+        address repayToken;
+        address borrowToken;
+        uint256 repayAmount;
+        uint256 borrowAmount;
+    }
     address internal immutable _FAKE_LENDING_PROTOCOL;
     address internal immutable _UNISWAP_ROUTER;
     address internal immutable _UNISWAP_FACTORY;
@@ -46,12 +54,33 @@ contract Liquidator is IUniswapV2Callee, Ownable {
 
     function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external override {
         // TODO
+        require(sender == address(this), "Sender must be this contract");
+        require(amount0 > 0 || amount1 > 0, "Amount must be greater than 0");
+        CallbackData memory callBackData = abi.decode(data, (CallbackData));
+
+        IERC20(callBackData.borrowToken).approve(_FAKE_LENDING_PROTOCOL, callBackData.borrowAmount);
+        IFakeLendingProtocol(_FAKE_LENDING_PROTOCOL).liquidatePosition();
+
+        address payable pairAddress = payable(IUniswapV2Factory(_UNISWAP_FACTORY).getPair(callBackData.borrowToken, callBackData.repayToken));
+
+        IWETH(_WETH9).deposit{ value: callBackData.repayAmount }();
+        IERC20(callBackData.repayToken).transfer(pairAddress, callBackData.repayAmount);        
     }
 
     // we use single hop path for testing
     function liquidate(address[] calldata path, uint256 amountOut) external {
         require(amountOut > 0, "AmountOut must be greater than 0");
         // TODO
+        address pair;
+        pair = IUniswapV2Factory(_UNISWAP_FACTORY).getPair(path[0], path[1]);
+        //base on amountOut, calculate how much amountIn we need
+        // uint256 amountIn;
+        uint256[] memory amountIn = IUniswapV2Router01(_UNISWAP_ROUTER).getAmountsIn(amountOut, path);
+        // call pair swap with address(this) uniswapV2Call function
+        
+        CallbackData memory callBackData = CallbackData(path[0], path[1], amountIn[0], amountOut);        
+        IUniswapV2Pair(pair).swap(0, amountOut, address(this), abi.encode(callBackData));
+
     }
 
     receive() external payable {}
